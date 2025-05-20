52a0e93dad7c3bcff96f1863048ff58bacc302ed
aravinddwara <149893688+aravinddwara@users.noreply.github.com>
2025-05-20 14:29:02 +0530

    Update Streamblasters.kt

--- Streamblasters/src/main/kotlin/com/Streamblasters/Streamblasters.kt
+++ Streamblasters/src/main/kotlin/com/Streamblasters/Streamblasters.kt
@@ -9,7 +9,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.utils.*
 
 class Streamblasters : MainAPI() {
-    override var mainUrl              = "https://www.streamblasters.pm"
+    override var mainUrl              = "https://www.tamildhool.net/"
     override var name                 = "Streamblasters"
     override val hasMainPage          = true
     override var lang                 = "hi"
@@ -19,19 +19,18 @@ class Streamblasters : MainAPI() {
 
     override val mainPage = mainPageOf(
         "" to "Latest",
-        "category/hindi" to "Hindi",
-        "category/english" to "English",
-        "category/tamil" to "Tamil",
-        "category/telugu" to "Telugu",
-        "category/malayalam" to "Malayalam",
-        "category/kannada" to "Kannada",
-        "category/web-series" to "Web Series"
-    )
+        "vijay-tv/vijay-tv-serial/baakiyalakshmi" to "Baakiyalakshmi",
+        "vijay-tv/vijay-tv-serial/siragadikka-aasai/" to "Siragadikka Aasai",
+         )
 
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("$mainUrl/${request.data}/page/$page/").document
-        val home     = document.select("div.blog-items > article").mapNotNull { it.toSearchResult() }
+     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+    val document = app.get("$mainUrl/${request.data}/page/$page/").document
 
+    // Target <section id="recent-posts"> and get its <article> children
+    val home = document.select("section#recent-posts > article")
+        .mapNotNull { it.toSearchResult() }
+
+        
         return newHomePageResponse(
             list    = HomePageList(
                 name               = request.name,
